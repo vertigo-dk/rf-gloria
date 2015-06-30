7963431524538673df78445f0eddea65b62c6c2b
Johan Bichel Lindegaard <public@johan.cc>
2015-06-30 15:17:57 +0200

    revert ports

--- Gloria/src/Defines.h
+++ Gloria/src/Defines.h
@@ -11,8 +11,8 @@
 //size of the output FBO's
 #define OUTWIDTH  5700 //5700
 #define OUTHEIGHT 1200 //1200
-#define OSCRECEIVEPORT 9000
-#define OSCSENDPORT 9001
+#define OSCRECEIVEPORT 9999
+#define OSCSENDPORT 8000
 #define OSCCLIENTONE "127.0.0.1" // Blue mac ip here
 #define GUIWIDTH 255
 #define TARGET_FRAMERATE 60
