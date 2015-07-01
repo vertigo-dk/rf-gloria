792962465a86d5acf2d14cbd37b70559e1abd17d
Obscura <obscura@REDmac.local>
2015-07-01 13:52:47 +0200

    fluid fix

--- Gloria/src/FluidScene.cpp
+++ Gloria/src/FluidScene.cpp
@@ -29,38 +29,7 @@ void FluidScene::setup(){
     fluid.end();
     
     obstacles.allocate(drawWidth, drawHeight);
-    /*
-    obstacles.begin();
-    
-    ofScale(1.0/scaleFactor, 1.0/scaleFactor);
-    ofBackground(0, 0, 0, 255);
-    
-    for(int i =0; i<mapping->triangles.size();i++) {
-        
-        ofSetColor(255);
-        //Corner * c1;
-        //c1 = mapping->triangles[i]->corners[(int)ofRandom(2)];
-        
-        ofSetLineWidth(10);
-        
-        for(int c=0; c<3; c++) {
-            ofVec3f pos = mapping->triangles[i]->corners[c]->pos;
-            ofVec3f nPos;
-            
-            if(c==2) {
-                nPos = mapping->triangles[i]->corners[0]->pos;
-            } else {
-                nPos = mapping->triangles[i]->corners[c+1]->pos;
-            }
-            
-            ofDrawLine(pos.getInterpolated(nPos, 0.1), nPos.getInterpolated(pos, 0.1));
-        }
-        
-        //ofFill();
-        //mapping->triangles[i]->mesh.drawFaces();
-        
-    }
-    obstacles.end();*/
+   
     
     //fluid.setObstacles(obstacles);
     fluid.setUseObstacles(false);
@@ -74,11 +43,12 @@ void FluidScene::setup(){
             temperatureDissipation.set("temperatureDissipation", 0.99, 0, 1),pressureDissipation.set("pressureDissipation", 0.99, 0, 1),clear.set("clear", false));
     
     drawObstacles = true;
+   
 }
 
 void FluidScene::update(){
     // Adding temporal Force
-    
+   
     fluid.dissipation = dissipation;
     fluid.velocityDissipation = velocityDissipation;
     fluid.temperatureDissipation = temperatureDissipation;
@@ -91,7 +61,7 @@ void FluidScene::update(){
     ofPoint d = (m - oldM)*10.0;
     oldM = m;
     
-    fluid.addTemporalForce(m, d, ofFloatColor(1, 1, 1),intensity);
+    fluid.addTemporalForce(m, d, ofFloatColor(0.8, 0.8, 0.8), intensity);
     
     // add velocity or add color based on image
     
@@ -99,7 +69,7 @@ void FluidScene::update(){
     //fluid.addTemporalForce(m, d, ofFloatColor(c.x,c.y,0.5)*sin(ofGetElapsedTimef()),3.0f);
     //  Update
     //
-    
+
     
     if(clear) {
         fluid.clear();
@@ -107,10 +77,12 @@ void FluidScene::update(){
         clear.set(false);
     }
     
+    ofPushStyle();
     //if(ofGetFrameNum() % 2 == 1) {
         fluid.update();
     //}
-    
+ 
+    ofPopStyle();
 }
 
 void FluidScene::draw(){;
