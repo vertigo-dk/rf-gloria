e25515361611b981a6b3b3b738d5bbbe9f456b87
Frederik Tollund Juutilainen <frederik.juutilainen@alexandra.dk>
2018-06-27 17:49:23 +0200

    added much needed icon

--- Gloria/src/main.cpp
+++ Gloria/src/main.cpp
@@ -3,8 +3,6 @@
 
 //--------------------------------------------------------------
 int main(){
-    //ofSetupOpenGL(1600, 800, OF_WINDOW);
-    
-	ofSetupOpenGL(1400,1200, OF_WINDOW);			// <-------- setup the GL context
-	ofRunApp(new ofApp()); // start the app
+	ofSetupOpenGL(1400,720, OF_WINDOW);
+	ofRunApp(new ofApp());
 }
